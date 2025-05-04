5b085a22cb2a75208569e3317a72fb07966cf62a
Νίκος Μιχελής <92666389+Nikos-Michelis@users.noreply.github.com>
2025-05-04 20:35:07 +0300

    Update logindb.sql

--- DataBase/logindb.sql
+++ DataBase/logindb.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Εξυπηρετητής: 127.0.0.1
--- Χρόνος δημιουργίας: 02 Οκτ 2022 στις 21:43:27
--- Έκδοση διακομιστή: 10.4.22-MariaDB
--- Έκδοση PHP: 7.4.27
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Βάση δεδομένων: `logindb`
---
-
--- --------------------------------------------------------
-
---
--- Δομή πίνακα για τον πίνακα `users`
---
 
 CREATE TABLE `users` (
   `usersId` int(11) NOT NULL,
@@ -37,7 +12,3 @@ CREATE TABLE `users` (
   `email_verified_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
